d47f1032f99f4de39b73974437ee16f337d1b906
Ravi S. Rāmphal <rramphal@gmail.com>
2018-07-24 17:33:39 -0700

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -17,7 +18,7 @@ const middlewares = process.env.NODE_ENV === 'development'
   ? applyMiddleware(thunk, logger)
   : applyMiddleware(thunk);
 
-const store = createStore(reducer, middlewares);
+const store = createStore(reducer, composeWithDevTools(middlewares));
 
 ReactDOM.render(
   <Provider store={store}>
